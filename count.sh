7d00854cdba4bea9e397a44aa214d07a12352b42
erikblank <erikblank97@web.de>
2021-04-19 15:49:39 +0200

    created count.sh

--- count.sh
+++ count.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+
+#download file
+wget -nv 'ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README'
+# to lowercase
+tr [:upper:] [:lower:] < README |
+# remove unnecessary spaces
+tr -s " " |
+#remove line break 
+tr -d "\n" |
+#remove digits
+tr -d [:digit:] |
+#remove punctuation
+tr -d [:punct:] |
+#split words per line
+sed 's/ /\n/g;' |
+#sort alphabetically
+sort |
+#remove duplicates and count occurances
+uniq -c |
+#sort reverse
+sort -r |
+#remove digits
+tr -d [:digit:] | 
+#stdout first 10 lines
+head 
+
+
 
